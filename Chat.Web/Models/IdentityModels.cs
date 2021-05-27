17d9d9e345fa21405ec3e4ffcb881ed7bcb6e750
Điển Vũ Khắc <vukhacdien196@gmail.com>
2021-05-27 10:17:33 +0700

    Add project files.

---
+++ Chat.Web/Models/IdentityModels.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Chat.Web.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string DisplayName { get; set; }
+
+        public string Avatar { get; set; }
+
+        public virtual ICollection<Room> Rooms { get; set; }
+        [InverseProperty("FromUser")]
+        public virtual ICollection<Message> FromUser { get; set; }
+
+        [InverseProperty("ToUser")]
+        public virtual ICollection<Message> ToUser { get; set; }
+
+        public virtual ICollection<UserRoom> UserRoom { get; set; }
+
+        
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<UserRoom> UserRooms { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
